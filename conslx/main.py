811893c794cd52c4d81d7d3251fa213b3b9ffd99
fmacvg <fly21@gmx.de>
2022-06-07 17:40:36 +0200

    not working fix

--- conslx/main.py
+++ conslx/main.py
@@ -1,3 +1,4 @@
+import array
 import os
 import time
 
@@ -10,8 +11,8 @@ class conslx:
         self.Bg = Bg
         self.MATRIX_GAME = []
         size = os.get_terminal_size()
-        self.HEIGHT = size[1]
-        self.WIDTH = size[0]
+        self.HEIGHT = size[0]
+        self.WIDTH = size[1]
         self.voxels = []
 
     def start(self):
@@ -33,20 +34,15 @@ class conslx:
                 print(l)
                 for i in range(l[1], l[1] + len(l)):
                     print(i)
-                    try:
-                        for z in range(l[0], l[0] + len(l[i])):
-                            try:
-                                if l[i + 2 - l[1]][z - l[0]] == " ":
-                                    i+=1
-                            except:
-                                i+=1
-                            else:
-                                try:
-                                    self.MATRIX_GAME[i][z] = l[i + 2 - l[1]][z - l[0]]
-                                except IndexError:
-                                    break
-                    except IndexError:
-                        pass
+                    for z in range(l[0], l[0] + len(l[i])):
+                        try:
+                            if l[i + 2 - l[1]][z - l[0]] == " ":
+                                i += 1
+                        except:
+                            i += 1
+                        else:
+                            self.MATRIX_GAME[i][z] = l[i + 2 - l[1]][z - l[0]]
+
             for i in self.MATRIX_GAME:
                 for z in i:
                     print(z, end="")
