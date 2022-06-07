b353104a06ac12d7df60bf61e585601f7c3453ef
fmacvg <fly21@gmx.de>
2022-06-07 16:59:51 +0200

    Initial commit

---
+++ conslx/main.py
@@ -0,0 +1,66 @@
+import os
+import time
+
+
+class conslx:
+    def __init__(self, GAME, sleep, Bg):
+        pass
+        self.GAME = GAME
+        self.sleep = sleep
+        self.Bg = Bg
+        self.MATRIX_GAME = []
+        size = os.get_terminal_size()
+        self.HEIGHT = size[1]
+        self.WIDTH = size[0]
+        self.voxels = []
+
+    def start(self):
+        self.MATRIX_GAME = [[0] * self.WIDTH for i in range(self.HEIGHT)]
+        for i in range(self.HEIGHT):
+            for y in range(self.WIDTH):
+                self.MATRIX_GAME[i][y] = self.Bg
+
+    def update(self):
+        while self.GAME:
+            for i in range(self.HEIGHT):
+                for y in range(self.WIDTH):
+                    self.MATRIX_GAME[i][y] = self.Bg
+            time.sleep(self.sleep)
+            os.system('cls' if os.name == 'nt' else 'clear')
+            # Get the size
+            # of the terminal
+            for l in self.voxels:
+                print(l)
+                for i in range(l[1], l[1] + len(l)):
+                    print(i)
+                    try:
+                        for z in range(l[0], l[0] + len(l[i])):
+                            try:
+                                if l[i + 2 - l[1]][z - l[0]] == " ":
+                                    i+=1
+                            except:
+                                i+=1
+                            else:
+                                try:
+                                    self.MATRIX_GAME[i][z] = l[i + 2 - l[1]][z - l[0]]
+                                except IndexError:
+                                    break
+                    except IndexError:
+                        pass
+            for i in self.MATRIX_GAME:
+                for z in i:
+                    print(z, end="")
+                print("")
+
+    def insert(self, obj, left, top):
+        obj.insert(0, left)
+        obj.insert(1, top)
+        self.voxels.append(obj)
+
+    class objekt:
+        def __init__(self, obj):
+            self.obj = obj
+
+        def create(self):
+            OBJ = [list(i) for i in self.obj.split('\n')]
+            return OBJ
---
+++ main.py
@@ -0,0 +1,28 @@
+import time
+
+from conslx.main import conslx
+from threading import Thread
+obj = """
+      ,~~.
+     (  9 )-_,
+(\___ )=='-'
+ \ .   ) )
+  \ `-' /
+   `~j-' 
+     "=:
+"""
+GAME = True
+Threads = []
+con = conslx(GAME=GAME, sleep=0.1, Bg=" ")
+con.start()
+def main():
+    con.update()
+duck = con.objekt(obj=obj).create()
+print(duck)
+process = Thread(target=main)
+process.start()
+con.insert(obj=duck,left=10,top=0)
+time.sleep(3)
+while True:
+    time.sleep(0.1)
+    con.voxels[0][0] +=1
